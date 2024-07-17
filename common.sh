b67e01819c72a8779a5e951a1b7676ea2616b01e
bhuvanrp <ravikumar.polina87@gmail.com>
2024-07-17 20:25:05 +0530

    added new file

--- common.sh
+++ common.sh
@@ -5,6 +5,6 @@ download_warfiles(){
 echo "download $component code"
 curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $log_file
 echo "Extract code"
-cd /usr/share/nginx/html
-unzip /tmp/$component.zip
+cd /usr/share/nginx/html >>log_file
+unzip /tmp/$component.zip >>log_file
 }
